0b2d62d373b041a7df731cc10b09f5c2593f7b35
skywanjer <ayannoorul@gmail.com>
2024-10-14 10:31:49 +0530

    day5

---
+++ day5/script.js
@@ -0,0 +1,45 @@
+// function display(){
+//     console.log("Bismillahir Rahmanir Rsahim");
+// // }
+// // display();
+// let x=12.23
+// let y="Hello";
+// let z=true;
+// let obj={
+//     'name':'Rahul',
+//     id:12,
+// }
+// let ar=[12,2,3,4,4]
+// let a;
+// let b=null;
+// console.log(typeof b);
+// console.log(typeof b);
+// console.log(y);
+// function validate(){
+//     let un =document.getElementById('un').value;
+//     let pass =document.getElementById('pass').value;
+//     let p1=document.getElementById('res');
+//     // alert('un');
+//     if(un=='admin'&& pass=='admin'){
+//      p1.innerHTML   ="Login Success";
+//     }
+//     else{
+//         p1.innerHTML="Login Failed";
+// }
+// }
+//Bismillahir Rahmair Rahim
+// let x=Number("12");
+// let str ="123";
+// // console.log(typeof x);
+// t a=2;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+
+// let say=()=> console.log("hello");
+// say();
+function display(){
+    let p1 =document.querySelector("p");
+    p1.innerHTML="Result";
+    p1.style.backgroundcolor="red";
+}
